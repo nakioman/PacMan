876e89156b33c2b54743ff5794967643472cc996
Ignacio Glinsek <iglinsek@gmail.com>
2016-05-10 14:52:02 -0300

    Add project files.

---
+++ PacMan/Engine/Player.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.InputListeners;
+using MonoGame.Extended.ViewportAdapters;
+
+namespace PacMan.Engine
+{
+    public class Player
+    {
+        private readonly ViewportAdapter _viewportAdapter;
+        private readonly InputListenerManager _inputListenerManager;
+        private Texture2D _spriteSheet;
+        private readonly IDictionary<string, SpriteAnimation> _animations;
+        private string _currentAnimation;
+        private KeyboardListener _keyboardListener;
+
+        public Player(ViewportAdapter viewportAdapter, InputListenerManager inputListenerManager)
+        {
+            _viewportAdapter = viewportAdapter;
+            _inputListenerManager = inputListenerManager;
+            _animations = new Dictionary<string, SpriteAnimation>();
+            _currentAnimation = "front";
+            _keyboardListener = _inputListenerManager.AddListener<KeyboardListener>();
+            _keyboardListener.KeyPressed += _keyboardListener_KeyPressed;
+            _keyboardListener.KeyReleased += KeyboardListenerOnKeyReleased;
+        }
+
+        private void KeyboardListenerOnKeyReleased(object sender, KeyboardEventArgs keyboardEventArgs)
+        {
+            _animations[_currentAnimation].Idle = true;
+        }
+
+        private void _keyboardListener_KeyPressed(object sender, KeyboardEventArgs keyboardEventArgs)
+        {
+            switch (keyboardEventArgs.Key)
+            {
+                case Keys.Left:
+                    _currentAnimation = "left";
+                    break;
+                case Keys.Right:
+                    _currentAnimation = "right";
+                    break;
+                case Keys.Up:
+                    _currentAnimation = "back";
+                    break;
+                case Keys.Down:
+                    _currentAnimation = "front";
+                    break;
+            }
+            _animations[_currentAnimation].Idle = false;
+        }
+
+        public void ContentLoad(ContentManager contet)
+        {
+            _spriteSheet = contet.Load<Texture2D>("Player");
+            _animations.Add("front", new SpriteAnimation(_viewportAdapter, _spriteSheet, 0, 0, 104, 64, 4));
+            _animations.Add("left", new SpriteAnimation(_viewportAdapter, _spriteSheet, 0, 104, 104, 64, 4));
+            _animations.Add("right", new SpriteAnimation(_viewportAdapter, _spriteSheet, 0, 208, 104, 64, 4));
+            _animations.Add("back", new SpriteAnimation(_viewportAdapter, _spriteSheet, 0, 312, 104, 64, 4));
+        }
+
+        public void UnloadContent()
+        {
+            _inputListenerManager.RemoveListener(_keyboardListener);
+            _animations.Clear();
+            _spriteSheet.Dispose();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _animations[_currentAnimation].Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _animations[_currentAnimation].Draw(spriteBatch);
+        }
+    }
+}
---
+++ PacMan/MainGame.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.InputListeners;
+using MonoGame.Extended.ViewportAdapters;
+using PacMan.Engine;
+
+namespace PacMan
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MainGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Player _player;
+        private ViewportAdapter _viewportAdapter;
+        private InputListenerManager _inputListenerManager;
+
+        public MainGame()
+        {
+            Window.AllowUserResizing = true;
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _viewportAdapter = new BoxingViewportAdapter(Window, graphics, 800, 400);
+            _inputListenerManager = new InputListenerManager(_viewportAdapter);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _player = new Player(_viewportAdapter, _inputListenerManager);
+            _player.ContentLoad(Content);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            _player.UnloadContent();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _inputListenerManager.Update(gameTime);
+            _player.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, null, null, null, null, _viewportAdapter.GetScaleMatrix());
+
+            _player.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
